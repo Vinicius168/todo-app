dfb313a58c8204fcfca08d543fb4032904f6b047
Vinicius <viniciusgames168@gmail.com>
2023-11-28 11:39:24 -0300

    iniciando o filtro de tarefas ativas

--- index.js
+++ index.js
@@ -72,6 +72,10 @@ app.post('/descompletar', (requisicao, resposta) => {
     })
 })
 
+app.get('/ativas', (requisicao, resposta) => {
+    
+})
+
 app.get('/', (requisicao, resposta) => {
     const sql = 'SELECT * FROM tarefas'
 
